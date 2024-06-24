7146c53c97819cae651a296d1279a2d58d87f258
Andre Lobo <loboandre@hotmail.com>
2024-06-24 16:42:46 -0400

    fix: CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Criar as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Inclui as rotas da API
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
+)
 app.include_router(api_router)
 
-# Rota raiz (opcional, para verificar se a API está funcionando)
+
 @app.get("/")
 def read_root():
     return {"message": "Olá IBBI !"}
